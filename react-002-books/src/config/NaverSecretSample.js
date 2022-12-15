9ccb479bb5c7e3464e10d0c89f6f3f3052cfb010
callor <callor@callor.com>
2022-12-15 11:15:03 +0900

    secret

---
+++ react-002-books/src/config/NaverSecretSample.js
@@ -0,0 +1,8 @@
+/**
+ * Naver 개발자 등록후 애플리케이션을 생성한다
+ * 생성된 애플리케이션의 CLIENT_ID 와 CLIENT_SECRET 를 아래 변수에
+ * 생성한 후
+ * NaverSecret.js 로 파일이름을 변경한 후 프로젝트를 시작하시오
+ */
+const CLIENT_ID = "";
+const CLIENT_SECRET = "";
