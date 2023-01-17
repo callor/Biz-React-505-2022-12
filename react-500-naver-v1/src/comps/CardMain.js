bb41308bed2b0d4db5e260c43989e9a0fa6f451b
callor <callor@callor.com>
2023-01-17 16:15:10 +0900

    naver

---
+++ react-500-naver-v1/src/comps/CardMain.js
@@ -0,0 +1,37 @@
+import "../css/CardMain.css";
+
+/**
+ * props.children 을 이용한 컴포넌트 합성
+ *
+ * 컴포넌트는 컴포는트를 포함하여 하나의 그룹을 형성한다
+ * 예) A 컴포넌트에 B 컴포넌트를 포함하는 형식의 컴포넌트 그룹을 만들면
+ * 만약 B 컴포넌를 다른 컴포넌트로 변경하면 A 컴포넌트를 사용하려면
+ * A 컴포넌트를 복제하여 중복된 코드를 발생시켜야 한다.
+ *
+ * 하지만 A 컴포넌트에 props.children 설정을 해 두고
+ * A 컴포넌트를 사용하는 곳에 다른 컴포넌트를 A 컴포넌트에 키워넣기 하여
+ * 마치 이미 만들어진 컴포너트처럼 사용하는 방법
+ *
+ */
+
+const CardMain = (props) => {
+  const mainStyle = {
+    width: props.width || "80%",
+    maxHeight: props.maxHeight || "300px",
+    margin: "20px auto",
+  };
+
+  return (
+    <div className="w3-card-4 Card-main" style={mainStyle}>
+      <header className="w3-container w3-center w3-sand w3-padding-24">
+        {props.header || <h1>나는 Card</h1>}
+      </header>
+      <section className="w3-contaner">{props.children}</section>
+      <footer className="w3-contaner w3-center w3-sand w3-padding-16">
+        hello
+      </footer>
+    </div>
+  );
+};
+
+export default CardMain;
---
+++ react-500-naver-v1/src/comps/books/BookList.js
@@ -0,0 +1,25 @@
+const Books = () => {
+  return (
+    <div className="w3-container w3-margin">
+      <ul>
+        <li>
+          <span>자바야 놀자</span>
+          <span>홍길동</span>
+          <span>광주출판</span>
+        </li>
+        <li>
+          <span>자바야 놀자</span>
+          <span>홍길동</span>
+          <span>광주출판</span>
+        </li>
+        <li>
+          <span>자바야 놀자</span>
+          <span>홍길동</span>
+          <span>광주출판</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Books;
