968e8ff5e96de93f2563e82c531358ad2f6220bb
callor <callor@callor.com>
2023-02-02 13:10:55 +0900

    drg

--- react-200-image-slide-v3/src/App.js
+++ react-200-image-slide-v3/src/App.js
@@ -3,27 +3,72 @@ import "./App.css";
 import { images } from "./modules/images";
 import { useState } from "react";
 
+const IMAGE_WIDTH = 500;
+
 function App() {
-  const [potion, setPosition] = useState(true);
+  const [potion, setPosition] = useState(0);
+  const [currentPos, setCurrentPos] = useState(0);
+  const [mouseX, setMouseX] = useState(0);
   const imagePrev = () => {
     setPosition(true);
   };
   const imageNext = () => {
     setPosition(false);
   };
+
+  const onDragHandler = (e) => {
+    // const box = e.currentTarget;
+    // const left = box.getBoundingClientRect().left;
+    // setPosition(e.pageX);
+    let newPotion = potion;
+    const newMouseX = e.clientX;
+
+    console.log(mouseX, newMouseX);
+    if (newMouseX < mouseX) {
+      newPotion = currentPos - newMouseX;
+    } else if (newMouseX > mouseX) {
+      newPotion = currentPos + newMouseX;
+    }
+    // setPosition(newPotion);
+    // setMouseX(newMouseX);
+  };
+
+  const onDragOverHandler = (e) => {
+    e.preventDefault();
+    e.currentTarget.style.opacity = "0.4";
+    setMouseX(e.clientX);
+  };
+
+  const onDragEndHandler = (e) => {
+    const box = e.currentTarget;
+    box.style.opacity = "1";
+    console.log("Drag End", e.clientX);
+    if (e.clientX < IMAGE_WIDTH * 0.5) {
+      setPosition(potion - IMAGE_WIDTH);
+    } else if (e.clientX > IMAGE_WIDTH * 0.5) {
+      setPosition(potion + IMAGE_WIDTH);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <div className="image-view">
+      <div
+        className="image-view"
+        draggable="true"
+        onDragOver={onDragOverHandler}
+        onDragEnd={onDragEndHandler}
+      >
         <div
-          className={
-            potion ? "image-box animation-left" : "image-box animation-right"
-          }
+          style={{ transform: `translateX(${potion}px)` }}
+          className="image-box"
+          draggable="false"
         >
           {images.map((image, index) => (
             <img
+              draggable="false"
               src={image}
               width="100%"
               height="100%"
@@ -33,6 +78,7 @@ function App() {
           ))}
           {images.map((image, index) => (
             <img
+              draggable="false"
               src={image}
               width="100%"
               height="100%"
